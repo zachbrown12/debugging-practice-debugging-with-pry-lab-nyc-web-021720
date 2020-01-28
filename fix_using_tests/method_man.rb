544de85bcb00be4099a67c28b94a64d04890c8a6
Zach Brown <zachmb22@gmail.com>
2020-01-28 04:17:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  puts "Hello #{player1} & #{playar2}"
 end
 
-def play_game(player2)
-  "#{player1} is better than #{player2}"
+def play_game(player1, player2)
+  puts "#{player1} is better than #{player2}"
 end
