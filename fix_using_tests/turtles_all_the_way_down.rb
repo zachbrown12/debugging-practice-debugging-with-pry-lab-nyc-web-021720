8d9dba469a099733695a3aec1b6fcd2c13e7fc20
Zach Brown <zachmb22@gmail.com>
2020-01-28 05:17:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -11,7 +11,7 @@ end
 def turtle_traits(turtles)
   newarray =[]
   turtles.map do |turtle|
-    newarray << turtle[:traits].split
+    if newarray << turtle[:traits]
   #  turtle.each do |key, value| 
    #   newarray << turtle[:traits]
       
