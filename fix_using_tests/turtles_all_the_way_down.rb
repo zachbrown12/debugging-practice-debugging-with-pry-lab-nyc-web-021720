490e11070bd8da77d4582bec3a9b294cc1e9bb8b
Zach Brown <zachmb22@gmail.com>
2020-01-28 05:10:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -12,8 +12,7 @@ def turtle_traits(turtles)
   
   turtles.map do |turtle|
     turtle.each do |key, value| 
-      if key == "traits:"
-        return "a"
+      return turtle[:traits]
       
     end
     end
